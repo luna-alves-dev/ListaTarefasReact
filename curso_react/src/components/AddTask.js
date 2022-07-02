703259e2c3f500f3a81632879d2b68db6ee36fb6
Luciana Alves <dra.luciana.alves@icloud.com>
2022-07-02 20:44:41 -0300

    Adiconado Button.cc e adicionado Button ao AddTask.js.

--- curso_react/src/components/AddTask.js
+++ curso_react/src/components/AddTask.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import "./AddTask.css";
+import Button from "./Button";
 
 const AddTask = () => {
   return (
     <div className="add-task-container">
     <input className="add-task-input" type="text" />;
+    <Button>Adicionar</Button>
     </div>
   );
 };
--- curso_react/src/components/Button.js
+++ curso_react/src/components/Button.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './Button.css';
+
 const Button = ({children, onClick}) => {
   return <Button onClick={onClick} className= "button">{children}</Button>
   };
